736628bf30db620a261d9051103db9bfaf99089b
Laurenda Edorh <laurenda98@gmail.com>
2023-04-06 02:13:40 +0200

    liaison des donnees au tableau

--- display.php
+++ display.php
@@ -1,5 +1,11 @@
+<?php
+
+include 'connect.php';
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="">
 
 <head>
     <meta charset="UTF-8">
@@ -7,7 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="syle.css">
-    <title>Crud opeartion</title>
+    <title>Crud operation</title>
 </head>
 
 <body>
@@ -15,54 +21,44 @@
         <h1>Operation Crud</h1>
     </div>
     <div class="conatainer">
-        <button type="" name="" class="btn btn-primary my-5"> <a class="text-light" href="user.php">Ajouter Utilisateur</a> </button>
-
-
+        <button type="" name="" class="btn btn-primary my-5 "> <a class="text-light" href="user.php">Ajouter Utilisateur</a> </button>
 
         <table class="table">
-            <thead class="thead-dark">
+            <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </tbody>
-        </table>
-
-        <table class="table">
-            <thead class="thead-light">
-                <tr>
-                    <th scope="col">Sl no</th>
-                    <th scope="col">Name</th>
+                    <th scope="col">Sl No</th>
+                    <th scope="col">Nom</th>
                     <th scope="col">Email</th>
                     <th scope="col">Mobile</th>
                     <th scope="col">Password</th>
-                    <th scope="col">Operations</th>
+                    <th scope="col">operations</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
+
+                <?php
+                $sql = "Select * from `crud`";
+                $result = mysqli_query($con, $sql);
+                if ($result) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id = $row['id'];
+                        $name = $row['name'];
+                        $email = $row['email'];
+                        $mobile = $row['mobile'];
+                        $password = $row['password'];
+                        echo '<tr>
+                        <th scope="row">' . $id . '</th>
+                        <td>' . $name . '</td>
+                        <td>' . $email . '</td>
+                        <td>' . $mobile . '</td>
+                        <td>' . $password . '</td>
+                    </tr>';
+                    }
+                }
+
+
+                ?>
+                <!-- <tr>
                     <th scope="row">1</th>
                     <td>Mark</td>
                     <td>Otto</td>
@@ -80,6 +76,7 @@
                     <td>the Bird</td>
                     <td>@twitter</td>
                 </tr>
+-->
             </tbody>
         </table>
     </div>
